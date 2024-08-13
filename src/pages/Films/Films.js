e6e4b186b191a9c0ceb40c3f8ad98c7b355b2637
Pazidu <you@example.com>
2024-08-14 00:10:11 +0530

    Changes in Films page

--- src/pages/Films/Films.js
+++ src/pages/Films/Films.js
@@ -1,30 +1,83 @@
-import React from 'react'
-import Navigation from '../../components/Navigation/Navigation'
-import FilmCard from '../../components/FilmCard/FilmCard'
+import React from "react";
+import Navigation from "../../components/Navigation/Navigation";
+import FilmCard from "../../components/FilmCard/FilmCard";
+import "./Films.css";
 
 export default function Films() {
   return (
     <div>
-        <Navigation 
-          title='Login'
-          profilePic='./images/profile.webp'
-          path='/Login'/>
+      <Navigation
+        title="Login"
+        profilePic="./images/profile.webp"
+        path="/Login"
+      />
 
+      <div className="firstRow">
         <FilmCard
-        image='./images/Films/garfield.jpg'
-        name='Garfield Movie'
-        imdbRate='5.7'
+          image="./images/Films/garfield.jpg"
+          name="Garfield Movie"
+          imdbRate="5.7"
         />
         <FilmCard
-        image='./images/Films/garfield.jpg'
-        name='Garfield Movie'
-        imdbRate='5.7'
+          image="./images/Films/garfield.jpg"
+          name="Garfield Movie"
+          imdbRate="5.7"
         />
         <FilmCard
-        image='./images/Films/garfield.jpg'
-        name='Garfield Movie'
-        imdbRate='5.7'
+          image="./images/Films/garfield.jpg"
+          name="Garfield Movie"
+          imdbRate="5.7"
         />
+        <FilmCard
+          image="./images/Films/garfield.jpg"
+          name="Garfield Movie"
+          imdbRate="5.7"
+        />
+      </div>
+      <div className="firstRow">
+        <FilmCard
+          image="./images/Films/garfield.jpg"
+          name="Garfield Movie"
+          imdbRate="5.7"
+        />
+        <FilmCard
+          image="./images/Films/garfield.jpg"
+          name="Garfield Movie"
+          imdbRate="5.7"
+        />
+        <FilmCard
+          image="./images/Films/garfield.jpg"
+          name="Garfield Movie"
+          imdbRate="5.7"
+        />
+        <FilmCard
+          image="./images/Films/garfield.jpg"
+          name="Garfield Movie"
+          imdbRate="5.7"
+        />
+      </div>
+      <div className="firstRow">
+        <FilmCard
+          image="./images/Films/garfield.jpg"
+          name="Garfield Movie"
+          imdbRate="5.7"
+        />
+        <FilmCard
+          image="./images/Films/garfield.jpg"
+          name="Garfield Movie"
+          imdbRate="5.7"
+        />
+        <FilmCard
+          image="./images/Films/garfield.jpg"
+          name="Garfield Movie"
+          imdbRate="5.7"
+        />
+        <FilmCard
+          image="./images/Films/garfield.jpg"
+          name="Garfield Movie"
+          imdbRate="5.7"
+        />
+      </div>
     </div>
-  )
+  );
 }
